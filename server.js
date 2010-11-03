8ed3348547299a5187b6b0e376411cadd925526e
Mike Cravey <techie@craveytrain.com>
2010-11-02 23:30:45 -0500

    Setting up control characters Storing the name on the server Styling the window just a slight bit

--- server.js
+++ server.js
@@ -18,13 +18,12 @@ function log (msg) {
   sys.puts(timestamp() + ' - ' + msg.toString());
 };
 
-var connections = 0;
+var connections = 0,
+		ctrlChar = '/';
 
 var port = 8000;
 
-var server = ws.createServer({
-	debug: true
-});
+var server = ws.createServer();
 
 server.addListener('listening', function (){
 	// log('Listening for connections @ http://localhost:' + port);
@@ -33,18 +32,27 @@ server.addListener('listening', function (){
 // Handle WebSocket Requests
 server.addListener('connection', function (conn) {
 	connections++;
-	server.broadcast('sys\t' + connections);
+	server.broadcast(ctrlChar + 'connCount\t' + connections);
 	// log('opened connection: ' + conn.id);
   
 	conn.addListener('message', function (message) {
-		// log('<'+conn.id+'> ' + message);
-		server.broadcast(timestamp() + '\t' + message);
+		if(message[0] == "/") {
+			message = message.substring(1).split('\t');
+			switch (message[0]) {
+				case 'name':
+					conn.storage.set("name", message[1]);
+					break;
+			}
+		} else {
+			// log('<'+conn.id+'> ' + message);
+			server.broadcast(timestamp() + '\t' + conn.storage.get("name") + '\t' + message);
+		}
 	});
 });
 
 server.addListener('close', function(conn){
 	connections--;
-	server.broadcast('sys\t' + connections);
+	server.broadcast(ctrlChar + 'connCount\t' + connections);
 	// log('closed connection: ' + conn.id);
 });
 
